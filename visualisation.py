05af131b32eca77a0c00a1485e298d70ad34e067
Jack Shee <jshee@student.unimelb.edu.au>
2017-10-07 16:41:32 +0800

    Visualisation edited with std showing and same axis

--- visualisation.py
+++ visualisation.py
@@ -4,15 +4,10 @@ import numpy as np
 #print(loan_data)
 
 #dummy data
-port_min = np.array([[100,110,120,130,140,150,160,170,180,190,200,210],
-            [200,190,180,170,160,150,140,130,120,110,100,90],
-            [300,310,320,330,340,350,360,370,380,390,400,410],
-            [500,510,520,530,540,550,560,570,580,590,500,610]])
-
-port_max = [[500,510,520,530,540,550,560,570,580,590,500,510],
-            [700,690,680,670,660,650,640,630,620,610,600,590],
-            [800,810,820,830,840,850,860,870,880,890,800,810],
-            [900,910,920,930,940,950,960,970,980,990,900,910]]
+port_std = np.array([[20,30,15,10,20,30,50,50,30,20,15,20],
+            [20,30,15,10,20,30,50,50,30,20,15,20],
+            [20,30,15,10,20,30,50,50,30,20,15,20],
+            [20,30,15,10,20,30,50,50,30,20,15,20]])
 
 port_avg = np.array([[300,310,320,330,340,350,360,370,380,390,300,310],
             [400,390,380,370,360,350,340,330,320,310,300,290],
@@ -40,11 +35,15 @@ def plot_portfolio_performance(t, mean, std):
 
     for i in range(1,4):
         model_min = np.min(mean[i]-std[i])
-        model_max = np.min(mean[i]+std[i])
+        model_max = np.max(mean[i]+std[i])
         if(model_min < ymin):
             ymin = model_min
         if(model_max > ymax):
             ymax = model_max
+
+    #give some buffer space
+    ymax = ymax + 100
+    ymin = ymin - 100
 
     #2x2 with 4 plots
     sub1 = fig.add_subplot(2,2,1)
@@ -60,7 +59,7 @@ def plot_portfolio_performance(t, mean, std):
     sub2.set_xlabel("Time (in months)")
     sub2.set_ylabel("$")
     sub2.plot(t,mean[1],'blue')
-    sub2.fill_between(t, mean[1]-std[1], mean[0]+ std[0],alpha=0.1, color = 'b')
+    sub2.fill_between(t, mean[1]-std[1], mean[1]+ std[1],alpha=0.1, color = 'b')
     sub2.set_ylim([ymin,ymax])
 
     sub3 = fig.add_subplot(2,2,3)
@@ -83,11 +82,10 @@ def plot_portfolio_performance(t, mean, std):
     plt.show()
 
 #test call
-print(type(port_avg[0]))
-plot_portfolio_performance(time,port_avg,port_min)
+plot_portfolio_performance(time,port_avg,port_std)
 
 #plot on the same axis
-def plot_portfolio_model_compare(t, port_avg):
+def plot_portfolio_model_compare(t, mean, std):
     """
     Generate a plot comparing portfolio performances over time given
     loan decisions made based on strategy chosen from varying models.
@@ -104,15 +102,20 @@ def plot_portfolio_model_compare(t, port_avg):
     """
     fig = plt.figure(figsize=(25,12))
 
-    plt.plot(t,port_avg[0],'black',label = "Model Name 1")
-    plt.plot(t,port_avg[1],'red',label = "Model Name 2")
-    plt.plot(t,port_avg[2],'blue',label = "Model Name 3")
-    plt.plot(t,port_avg[3],'green',label = "Model Name 4")
+    plt.plot(t, mean[0],'red',label = "Model Name 1")
+    plt.fill_between(t, mean[0]-std[0], mean[0]+ std[0],alpha=0.1, color = 'r')
+    plt.plot(t, mean[1],'blue',label = "Model Name 2")
+    plt.fill_between(t, mean[1]-std[1], mean[1]+ std[1],alpha=0.1, color = 'b')
+    plt.plot(t,port_avg[2],'green',label = "Model Name 3")
+    plt.fill_between(t, mean[2]-std[2], mean[2]+ std[2],alpha=0.1, color = 'g')
+    plt.plot(t,port_avg[3],'black',label = "Model Name 4")
+    plt.fill_between(t, mean[3]-std[3], mean[3]+ std[3],alpha=0.1, color = 'k')
     plt.xlabel("Time (in months)")
+    plt.legend(loc = "upper right")
     plt.ylabel("$")
-    plt.title("Comparison of Mean Portfolio Performance")
+    plt.title("Comparison of Portfolio Performance")
 
     plt.show()
 
 #test call
-plot_portfolio_model_compare(time,port_avg)
+plot_portfolio_model_compare(time,port_avg, port_std)
