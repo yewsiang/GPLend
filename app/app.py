bb4715823c1dad081b391597b207fd9c18f5537a
yewsiang <tangys108@gmail.com>
2017-11-10 20:54:14 +0800

    Visualize graphs

--- app/app.py
+++ app/app.py
@@ -56,5 +56,20 @@ def run():
 
 	# Compute simulation
 	results = [10.2, 20.3]
+
+	import matplotlib.pyplot as plt
+	import numpy as np
+	import mpld3
+	# Scatter points
+	fig, ax = plt.subplots()
+	np.random.seed(0)
+	x, y = np.random.normal(size=(2, 200))
+	color, size = np.random.random((2, 200))
+
+	ax.scatter(x, y, c=color, s=500 * size, alpha=0.3)
+	ax.grid(color='lightgray', alpha=0.7)
+	results = mpld3.fig_to_html(fig)
+	
+	print(results)
 	
 	return render_template('index.html', results=results)
